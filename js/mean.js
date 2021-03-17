72679e2f616c4ddacdc3fbb6855e814916c6e96d
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-17 19:51:49 +0300

    add

--- js/mean.js
+++ js/mean.js
@@ -1,14 +1,2 @@
-function add() {
-    let sum = [];
-    for (let i = 0; i < arguments.length; i++) {
-        sum.push(arguments[i])
-        
-    }
-    a = sum.sort()
-    d = a.length
-    console.log(a[d-1])
-
-  }
-  
-   add(2, 4);
-  
+//  
+   
